d0fe13743f9bec40281efc13d67283092cea2937
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-19 21:23:10 -0800

    Added comments.

--- src/utilTool.js
+++ src/utilTool.js
@@ -1,41 +1,45 @@
 exports.debug = (title, obj) => {
-  const colors = require('colors');
-  const fs = require('fs');
+  const colors = require('colors'); // Colors package to style the message.
 
-  const border = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
-  const time = new Date();
-  const theme = {
+  const border = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n'; // Message border.
+  const time = new Date(); // Current Date.
+  const theme = { // Color themes.
     create: 'green',
     read: 'yellow',
     update: 'magenta',
     delete: 'red',
     error: 'red',
   };
-  colors.setTheme(theme);
+  colors.setTheme(theme); // Setup color themes.
 
-  let level;
+  let level; // Let to hold level.
 
-  Object.keys(theme).forEach((key) => {
-    const color = theme[key];
-    const colorCode = colors.styles[color].open;
+  Object.keys(theme).forEach((key) => { // For each theme.
+    const color = theme[key]; // Grab the color.
+    const colorCode = colors.styles[color].open; // Grab the color code from the colors library.
 
-    const colorCodeArr = colorCode.split('');
-    colorCodeArr.splice(0, 1);
+    const colorCodeArr = colorCode.split(''); // Split each character apart for the library color code.
+    colorCodeArr.splice(0, 1); // Remove the console escape section from the beginning.
 
-    const titleArr = title.split('');
-    titleArr.splice(0, 1);
-    titleArr.splice(4);
+    const titleArr = title.split(''); // Split each character apart for the title.
+    titleArr.splice(0, 1); // Remove the console escape section from the beginning.
+    titleArr.splice(4); // Splice out the color code.
 
-    if (colorCodeArr.toString() === titleArr.toString()) {
-      level = key;
+    if (colorCodeArr.toString() === titleArr.toString()) { // If the string of the two color code arrays match.
+      level = key; // Set the key/level to level.
     }
   });
 
-  console.log(level);
+  const output = border + title + JSON.stringify(obj) + '\n' + time + border; // Concat the output.
 
-  const output = border + title + JSON.stringify(obj) + '\n' + time + border;
-
-  if (process.env.DEBUG) {
-    console.log(output);
+  if (process.env.DEBUG) { // If debug is set.
+    switch (level) { // If the level is...
+      case 'error': // Error, output to the error console.
+        console.error(output);
+        break;
+      default: // Otherwise output to the regular console.
+        console.log(output);
+        break;
+    }
   }
 };
