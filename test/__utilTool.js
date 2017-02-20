dc6597670df856556227102c624f23cb46b1eb34
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-19 21:48:18 -0800

    Adding basic tests for logger.

--- test/__utilTool.js
+++ test/__utilTool.js
@@ -1,3 +1,22 @@
+const rewire = require('rewire');
+const util = rewire('../src/utilTool.js');
+
+
+
 describe('Util Tool DEBUG', () => {
-  it('Should write to file', () => {})
+  it('Should write to file', () => {});
+  describe('No Output When DEBUG false.', () => {
+    it('No message should be output', () => {
+      util.debug('test', { test: 'object' });
+    });
+  });
+  describe('Output Message Test When DEBUG True.', () => {
+    beforeEach(() => {
+      util.__set__('process.env.DEBUG', 'true');
+    });
+    it('Output a message.', () => {
+      util.debug('test'.update, { test: 'object' });
+      util.debug('test'.error, { test: 'object' });
+    });
+  });
 });
