f82622ced0fa04fea98972fa7a185746603b6967
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-19 21:08:07 -0800

    Determined the message level based on the color assigned.

--- src/utilTool.js
+++ src/utilTool.js
@@ -4,13 +4,35 @@ exports.debug = (title, obj) => {
 
   const border = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
   const time = new Date();
-  colors.setTheme({
+  const theme = {
     create: 'green',
     read: 'yellow',
     update: 'magenta',
     delete: 'red',
     error: 'red',
+  };
+  colors.setTheme(theme);
+
+  let level;
+
+  Object.keys(theme).forEach((key) => {
+    const color = theme[key];
+    const colorCode = colors.styles[color].open;
+
+    const colorCodeArr = colorCode.split('');
+    colorCodeArr.splice(0, 1);
+
+    const titleArr = title.split('');
+    titleArr.splice(0, 1);
+    titleArr.splice(4);
+
+    if (colorCodeArr.toString() === titleArr.toString()) {
+      level = key;
+    }
   });
+
+  console.log(level);
+
   const output = border + title + JSON.stringify(obj) + '\n' + time + border;
 
   if (process.env.DEBUG) {
